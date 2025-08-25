026667f542141795de531a4d7d247b7811ba66c5
nin-jin <nin-jin@users.noreply.github.com>
2025-08-25 07:20:53 +0000

    Deploying to gh-pages from @ hyoo-ru/bunker.hyoo.ru@9be6e7de33bb845937b0745631ce91c739fb8253 🚀

--- web.js
+++ web.js
@@ -9988,7 +9988,7 @@ var $;
 (function ($) {
     $.$hyoo_crus_vary_mapping = {
         nil: null,
-        bin: Uint8Array,
+        bin: (Uint8Array),
         bool: Boolean,
         int: BigInt,
         real: Number,
