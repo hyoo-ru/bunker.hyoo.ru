e13978cad351c158c63173ef06680c132ec52460
nin-jin <nin-jin@users.noreply.github.com>
2026-02-09 07:58:07 +0000

    Deploying to gh-pages from @ hyoo-ru/bunker.hyoo.ru@9be6e7de33bb845937b0745631ce91c739fb8253 🚀

--- node.d.ts
+++ node.d.ts
@@ -3098,6 +3098,7 @@ declare namespace $ {
         native(): Promise<CryptoKey>;
         native_derive(): Promise<CryptoKey>;
         verify(data: BufferSource, sign: BufferSource): Promise<boolean>;
+        toJSON(): string;
     }
     class $mol_crypto_key_private extends $mol_crypto_key {
         static size_str: number;
@@ -3108,6 +3109,7 @@ declare namespace $ {
         native_derive(): Promise<CryptoKey>;
         public(): $mol_crypto_key_public;
         sign(data: BufferSource): Promise<Uint8Array<ArrayBuffer>>;
+        toJSON(): string;
     }
 }
 
--- web.d.ts
+++ web.d.ts
@@ -3047,6 +3047,7 @@ declare namespace $ {
         native(): Promise<CryptoKey>;
         native_derive(): Promise<CryptoKey>;
         verify(data: BufferSource, sign: BufferSource): Promise<boolean>;
+        toJSON(): string;
     }
     class $mol_crypto_key_private extends $mol_crypto_key {
         static size_str: number;
@@ -3057,6 +3058,7 @@ declare namespace $ {
         native_derive(): Promise<CryptoKey>;
         public(): $mol_crypto_key_public;
         sign(data: BufferSource): Promise<Uint8Array<ArrayBuffer>>;
+        toJSON(): string;
     }
 }
 
